8edbf7730b45e2b9bbc7268c2791b024f0f7080a
BirdMakingStuff <github@birdmakingstuff.nz>
2025-04-02 16:12:53 +1300

    Anonymous posts by API key holder should no longer leak poster

--- src/ed/ed-embed.js
+++ src/ed/ed-embed.js
@@ -5,7 +5,7 @@ export default function EdEmbed(threadObj) {
         .setColor('#50288c')
         .setTitle(threadObj.title)
         .setURL(`https://edstem.org/au/courses/${threadObj.course_id}/discussion/${threadObj.id}`)
-        .setAuthor((threadObj.user_id === 0) ? {name: `Anonymous #${threadObj.anonymous_id}`} : {name: threadObj.user.name, iconURL: (threadObj.user.avatar === null) ? null : `https://static.au.edusercontent.com/avatars/${threadObj.user.avatar}?s=128&fallback=1`}  )
+        .setAuthor((threadObj.user_id === 0 || threadObj.is_anonymous) ? {name: `Anonymous #${threadObj.anonymous_id}`} : {name: threadObj.user.name, iconURL: (threadObj.user.avatar === null) ? null : `https://static.au.edusercontent.com/avatars/${threadObj.user.avatar}?s=128&fallback=1`}  )
         .setDescription((threadObj.document.length > 4096) ? threadObj.document.substring(0, 4096) : threadObj.document)
         .setFooter({text: (threadObj.type === 'question') ? `Question #${threadObj.number}` : `Post #${threadObj.number}`})
         .setTimestamp(Date.parse(threadObj.created_at));
