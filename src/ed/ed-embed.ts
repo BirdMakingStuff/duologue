77265de98126fbbb9a6d3c2a91b78cc6f1d655f8
hsma729 <hsma729@aucklanduni.ac.nz>
2026-08-13 19:28:00 +1200

    resolved this #22

--- src/ed/ed-embed.ts
+++ src/ed/ed-embed.ts
@@ -1,6 +1,20 @@
 import { EmbedBuilder, EmbedAuthorOptions } from 'discord.js';
 import type { Thread } from './ed-handler.js';
 
+
+const ED_ANIMALS = [
+    'Overcaffeinated Goblin',
+    'Degree Regretter',
+    'All-Nighter Champion',
+    'Code Compiler Pray-er',
+    'Deadline Dodger',
+    'StackOverflow Copy-Paster',
+    'Group Project Carry',
+    'Tears-in-the-Lab Enabler',
+    'ChatGPT Dependent',
+    'Procrastination Legend'
+];
+
 export default function EdEmbed(threadObj: Thread): EmbedBuilder {
     const embed = new EmbedBuilder()
         .setColor('#50288c')
@@ -10,9 +24,21 @@ export default function EdEmbed(threadObj: Thread): EmbedBuilder {
         .setFooter({ text: threadObj.type === 'question' ? `Question #${threadObj.number}` : `Post #${threadObj.number}` })
         .setTimestamp(Date.parse(threadObj.created_at));
 
-    const authorName = threadObj.user_id === 0 || threadObj.is_anonymous
-        ? `Anonymous #${threadObj.anonymous_id ?? 'N/A'}`
-        : threadObj.user?.name;
+    // 2. Updated author logic to inject your funny titles
+    let authorName = threadObj.user?.name;
+
+    if (threadObj.user_id === 0 || threadObj.is_anonymous) {
+        const anonId = threadObj.anonymous_id;
+        
+        if (anonId !== undefined && anonId !== null) {
+            // Pick a funny name consistently using the ID math
+            const animalIndex = anonId % ED_ANIMALS.length;
+            const animal = ED_ANIMALS[animalIndex];
+            authorName = `Anonymous ${animal} (#${anonId})`;
+        } else {
+            authorName = 'Anonymous User';
+        }
+    }
 
     if (authorName) {
         const author: EmbedAuthorOptions = {
